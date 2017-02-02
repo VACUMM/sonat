c7f3dbcc759490fdb56f2ed91e24963d7739e1b2
Stephane Raynaud <stephane.raynaud@gmail.com>
2017-02-02 13:29:43 +0100

    Fix os import in my

--- sonat/my.py
+++ sonat/my.py
@@ -34,7 +34,7 @@ User defined stuff
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL license and that you accept its terms.
 #
-
+import os
 import imp
 import inspect
 
